8c823c1956203c811d83318308c445567f43eeba
Carl Furrow <me@carlfurrow.com>
2012-03-09 21:41:01 -0500

    Using a callback to handle what I get back from lat/lng - just outputting to console

--- js/modules/geocacher.js
+++ js/modules/geocacher.js
@@ -1,21 +1,19 @@
 
-var getLagLng = function(){
-  var lat = 0;
-  var lng = 0;
-  if(navigation.geolocation){
+var getLatLng = function(callback){
+  if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(pos){
-      lat = pos.coords.latitude;
-      lng = pos.coords.longitude;
+      callback.apply(pos,[pos.coords.latitude,pos.coords.longitude]);
     });
   }
-  return {lat:lat,lng:lng};
 };
 
 var init = function(){
   console.log("geocacher.js - init");
-  var latlng = getLatLng();
-
-  console.log("Current Location: " + latlng.lat + ", " + latlng.lng);
+  getLatLng(outputLatLngToConsole);
+};
+var outputLatLngToConsole = function(lat,lng){
+  console.log("Lat: " + lat);
+  console.log("Lng: " + lng);
 };
 
 exports.getLatLng = this.getLatLng;
