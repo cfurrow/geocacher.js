d472f12cbc4230dcc09e2bdeec8bb4e642141de9
Carl Furrow <me@carlfurrow.com>
2012-03-10 17:30:26 -0500

    Logging lat,lng and accuracy in meters.

--- js/modules/main.js
+++ js/modules/main.js
@@ -9,10 +9,10 @@ var map, latlng, marker, accuracyInMeters;
 var setupMap = function(lat,lng,position){
   map = new mxn.Mapstraction('mapdiv','openlayers');
   latlng = new mxn.LatLonPoint(lat,lng);
-  console.log("Lat: " + latlng.lat + " Lng: " + latlng.lng);
+  accuracyInMeters = position.coords.accuracy;
+  console.log("Lat: " + latlng.lat + " Lng: " + latlng.lng + " Accuracy (meters): "+accuracyInMeters);
   map.setCenterAndZoom(latlng,15);
 
-  accuracyInMeters = position.coords.accuracy;
   var accuracyInMiles = metersToMiles(accuracyInMeters);
 
   dropMarkerAndRadius(map,latlng,accuracyInMiles);
@@ -24,15 +24,10 @@ var dropMarkerAndRadius = function(map,latlng,radius){
   map.addMarker(marker);
 
   var r = new mxn.Radius(latlng,20);
-  console.log("Radius",r);
 
   var polyline = r.getPolyline(radius,"#000fff");
   polyline.setClosed(true);
-  console.log("Polyline",polyline);
   map.addPolyline(polyline);
-
-  
-
 };
 
 getLatLng(setupMap);
