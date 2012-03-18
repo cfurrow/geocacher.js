bc454941be453cedf43606d985ab43799da932ff
Carl Furrow <me@carlfurrow.com>
2012-03-18 17:14:30 -0400

    dropMarkerAndRadius returns marker obj

--- js/modules/marker.js
+++ js/modules/marker.js
@@ -1,15 +1,14 @@
-var markers = [];
-
 var dropMarkerAndRadius = function(map,latlng,radius){
   var marker = new mxn.Marker(latlng);
   map.addMarker(marker);
-  markers.push[markers];
 
   var r = new mxn.Radius(latlng,15);
 
   var polyline = r.getPolyline(radius,"#000fff");
   polyline.setClosed(true);
   map.addPolyline(polyline);
+
+  return marker;
 };
 
 exports.dropMarkerAndRadius = dropMarkerAndRadius;
