561a5a7fef627c4d957017d8ae3dd7ea98c15423
Carl Furrow <me@carlfurrow.com>
2012-03-23 20:52:26 -0400

    Cleaned up closure issues in history.view - on deleteClick, closure is now correct!! - that was stupid

--- js/modules/history.view.js
+++ js/modules/history.view.js
@@ -1,5 +1,7 @@
 var storage = require('storage');
-var mapstraction = require('mapstraction').mapstraction;
+var mapstraction = require('mapstraction');
+var marker = require('marker');
+var markers = marker.markers; 
 var $historyContainer = $("#position-history");
 
 var addNewHistoryRow = function(html){
@@ -10,16 +12,17 @@ var deleteClick = function(eventObj){
   eventObj.preventDefault();
   var $link = $(this);
   var key = $link.attr("data-key");
+  $link.parents("tr#"+key).remove();
+  return key;
+};
 
+$("body").delegate(".point .link-delete","click",function(e){
+  var key = deleteClick.call(this,e);
   storage.get(key,function(point){
     storage.remove(point.key);
-    $link.parents("tr#"+point.key).remove();
-    if(point.marker){
-      mapstraction.removeMarker(point.marker);
-    }
+    marker.removeMarkerAndRadius(point.marker);
   });
-};
-$("body").delegate(".point .link-delete","click",deleteClick);
+});
 
 exports.addNewHistoryRow = addNewHistoryRow;
 exports.addMarkerCallback = function(){};
