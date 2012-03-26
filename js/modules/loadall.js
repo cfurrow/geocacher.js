a270c30d70d9168ea0e3dad95b5ecc3b64653a78
Carl Furrow <me@carlfurrow.com>
2012-03-26 11:05:13 -0400

    adding a loadall.js module

---
+++ js/modules/loadall.js
@@ -0,0 +1,9 @@
+var animatedMarker = require('animated.marker');
+var formatter = require('formatter');
+var history = require('history');
+var hisotoryView = require('history.view');
+var mapstraction = require('mapstraction');
+var marker = require('marker');
+var navigator = require('navigator');
+var storage = require('storage');
+
